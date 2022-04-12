a7f6a499d19a0ddc286a94b1b4018e692aa3b6d1
Mahbubur Rashid <mahbuburrashidusa@gmail.com>
2022-04-11 23:58:09 -0400

    this is my sample5 message

---
+++ src/rashid10/LearnPremitive10.java
@@ -0,0 +1,65 @@
+package rashid10;
+
+public class LearnPremitive10 {
+	
+	
+
+
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -127;
+		short salary = 5088;
+		
+		System.out.println(num); // calling a variable
+		
+		System.out.println ("Mahbub");
+		System.out.println(firstName); //calling a variable
+		System.out.println(lastName);  //calling a variable
+		
+		System.out.println(number);
+		
+		
+		System.out.println(salary);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/rashid9/LearnPremitive9.java
@@ -0,0 +1,63 @@
+package rashid9;
+
+public class LearnPremitive9 {
+	
+	
+	
+
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -127;
+		short salary = 5077;
+		
+//		System.out.println(num); // calling a variable
+//		
+//		System.out.println ("Mahbub");
+	System.out.println("My first Name:"+"\t"+firstName +" \n"+"my last name:"+"\t"+lastName); //calling a variable
+//		//System.out.println(lastName);  //calling a variable
+//		
+//		System.out.println(number);
+//		
+//		
+//		System.out.println(salary);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
